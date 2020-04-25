c61e4847262f1e46b68c74965a5f92ad02685fb0
artem <artall64@gmail.com>
2020-04-25 17:21:29 +0300

    add timer demo from codepen

---
+++ src/app/animated-number/animated-number.component.ts
@@ -0,0 +1,109 @@
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-animated-number',
+  templateUrl: './animated-number.component.html',
+  styleUrls: ['./animated-number.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AnimatedNumberComponent implements OnInit, OnChanges {
+
+  title = 'animated-number';
+
+  @Input()
+  value: number = 1111;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.doRendering();
+  }
+
+  ngOnInit(): void {
+  }
+
+  private doRendering() {
+
+    //
+    const h3counter = document.querySelector('#counter');
+    const h3counterParent = h3counter.parentNode;
+    h3counter.parentNode.removeChild(h3counter);
+    //
+
+    const h3 = document.createElement('h3');
+
+    const valueToRender = this.value.toString().padStart(6, '0');
+    h3.setAttribute('aria-label', valueToRender);
+    h3.id = 'counter';
+    h3.className = 'counter';
+    h3.textContent = valueToRender;
+    h3.style.maxHeight = '60px';
+    h3counterParent.appendChild(h3);
+
+    const stats = document.querySelectorAll(".counter");
+    const x = this.value;
+
+    stats.forEach(stat => {
+      // pattern used to seperate input number from html into an array of numbers and non numbers. EX $65.3M -> ["$65.3M", "$", "65", ".", "3", "M"]
+      const patt = /(\D+)?(\d+)(\D+)?(\d+)?(\D+)?/;
+      const time = 1000;
+      let result = [...patt.exec(stat.textContent)];
+      let fresh = true;
+      let ticks;
+
+      // Remove first full match from result array (we dont need the full match, just the individual match groups).
+      result.shift();
+
+      // Remove undefined values from result array where they didnt have a match in one of the optional regex groups
+      result = result.filter(res => res != null);
+
+      while (stat.firstChild) {
+        stat.removeChild(stat.firstChild);
+      }
+
+      for (const res of result) {
+        // if (isNaN(res as any)) {
+        //   stat.insertAdjacentHTML("beforeend", `<span>${res}</span>`);
+        // } else {
+          // debugger
+          for (let i = 0; i < res.length; i++) {
+            stat.insertAdjacentHTML(
+              "beforeend",
+              `<span data-value="${res[i]}">
+						<span>&nbsp;</span>
+						${Array(parseInt(res[i]) + 1)
+                .join('0')
+                .split('0')
+                .map(
+                  (x, j) => `
+							<span>${j}</span>
+						`
+                )
+                .join("")}
+					</span>`
+            );
+          }
+        //}
+      }
+
+
+      let activate = () => {
+        // debugger
+        let top = stat.getBoundingClientRect().top;
+        let offset = window.innerHeight * 3 / 4;
+        ticks = [
+          ...((stat as any).querySelectorAll("span[data-value]"))
+        ];
+
+        // if (top < offset) {
+        //   setTimeout(() => {
+            for (let tick of ticks) {
+              let dist = parseInt(tick.getAttribute("data-value")) + 1;
+              tick.style.transform = `translateY(-${dist * 100}%)`;
+            }
+          // }, 1000);
+        // }
+      };
+
+      activate();
+    });
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = 'animated-number';
+export class AppComponent implements OnInit {
+
+  superValue = 100500;
+
+  ngOnInit(): void {
+    // setInterval(() => {
+    //   this.superValue = 239112;
+    // }, 10000)
+  }
+
+  onclick(e: any) {
+    const x = e.clientX;
+    const y = e.clientY;
+    const s = (Math.random() * 1000000).toFixed(0).padStart(6, '0');
+    this.superValue = +s;
+    //console.log(s);
+    //this.superValue
+  }
+
+
+  // title = 'animated-number';
+  //
+  // @Input()
+  // value: number
+  //
+  // ngOnInit(): void {
+  //   //
+  //   const stats = document.querySelectorAll(".counter");
+  //
+  //   //
+  //   stats.forEach(stat => {
+  //     // pattern used to seperate input number from html into an array of numbers and non numbers. EX $65.3M -> ["$65.3M", "$", "65", ".", "3", "M"]
+  //     const patt = /(\D+)?(\d+)(\D+)?(\d+)?(\D+)?/;
+  //     const time = 1000;
+  //     let result = [...patt.exec(stat.textContent)];
+  //     let fresh = true;
+  //     let ticks;
+  //
+  //     // Remove first full match from result array (we dont need the full match, just the individual match groups).
+  //     result.shift();
+  //
+  //     // Remove undefined values from result array where they didnt have a match in one of the optional regex groups
+  //     result = result.filter(res => res != null);
+  //
+  //     while (stat.firstChild) {
+  //       stat.removeChild(stat.firstChild);
+  //     }
+  //
+  //     for (const res of result) {
+  //       if (isNaN(res as any)) {
+  //         stat.insertAdjacentHTML("beforeend", `<span>${res}</span>`);
+  //       } else {
+  //         // debugger
+  //         for (let i = 0; i < res.length; i++) {
+  //           stat.insertAdjacentHTML(
+  //             "beforeend",
+  //             `<span data-value="${res[i]}">
+  // 					<span>&ndash;</span>
+  // 					${Array(parseInt(res[i]) + 1)
+  //               .join(0)
+  //               .split(0)
+  //               .map(
+  //                 (x, j) => `
+  // 						<span>${j}</span>
+  // 					`
+  //               )
+  //               .join("")}
+  // 				</span>`
+  //           );
+  //         }
+  //       }
+  //     }
+  //
+  //
+  //     let activate = () => {
+  //       // debugger
+  //       let top = stat.getBoundingClientRect().top;
+  //       let offset = window.innerHeight * 3 / 4;
+  //       ticks = [
+  //         ...((stat as any).querySelectorAll("span[data-value]"))
+  //       ];
+  //
+  //       // setTimeout(() => {
+  //       // 	fresh = false;
+  //       // }, time);
+  //
+  //       if (top < offset) {
+  //         setInterval(() => {
+  //           for (let tick of ticks) {
+  //             let dist = parseInt(tick.getAttribute("data-value")) + 1;
+  //             tick.style.transform = `translateY(-${dist * 100}%)`;
+  //           }
+  //         }, 1000);
+  //         //window.removeEventListener("scroll", activate);
+  //       }
+  //     };
+  //     //window.addEventListener("scroll", activate);
+  //     activate();
+  //     setTimeout(() => activate(), 3000)
+  //   });
+  //
+  // }
 }
